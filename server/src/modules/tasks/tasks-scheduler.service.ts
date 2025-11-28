be9651f3d739e0980f0ddbc058494407da45a981
nekit <garkusha500@gmail.com>
2025-11-29 00:07:01 +0300

    n

--- server/src/modules/tasks/tasks-scheduler.service.ts
+++ server/src/modules/tasks/tasks-scheduler.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+// import { Cron } from '@nestjs/schedule';
 import { UsersService } from '../users/users.service';
 import { TasksService } from '../tasks/tasks.service';
 import { TelegramService } from '../telegram/telegram.service';
@@ -15,19 +15,18 @@ export class TasksSchedulerService {
   ) {}
 
   // @Cron('*/1 * * * *')
-  @Cron('0 9 * * *')
+  // @Cron('0 9 * * *')
   async sendDailyTasks() {
-    const users = await this.usersService.getAllWithUserRole();
-
-    for (const user of users) {
-      const exercise = await this.exercisesService.getRandom();
-      const task = await this.tasksService.create(user.id, exercise.text);
-      if (user.telegramId) {
-        await this.telegramService.sendMessage(
-          user.telegramId,
-          `Ваше задание на сегодня: ${task.text}\nНомер задания: ${task.shortId}`,
-        );
-      }
-    }
+    // const users = await this.usersService.getAllWithUserRole();
+    // for (const user of users) {
+    //   const exercise = await this.exercisesService.getRandom();
+    //   const task = await this.tasksService.create(user.id, exercise.text);
+    //   if (user.telegramId) {
+    //     await this.telegramService.sendMessage(
+    //       user.telegramId,
+    //       `Ваше задание на сегодня: ${task.text}\nНомер задания: ${task.shortId}`,
+    //     );
+    //   }
+    // }
   }
 }
--- server/src/modules/tasks/tasks.module.ts
+++ server/src/modules/tasks/tasks.module.ts
@@ -1,7 +1,7 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { TelegramModule } from '../telegram/telegram.module';
+// import { TelegramModule } from '../telegram/telegram.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { TasksSchedulerService } from './tasks-scheduler.service';
 import { UsersModule } from '../users/users.module';
@@ -9,7 +9,7 @@ import { ExercisesModule } from '../exercises/exercises.module';
 
 @Module({
   imports: [
-    forwardRef(() => TelegramModule),
+    // forwardRef(() => TelegramModule),
     PrismaModule,
     UsersModule,
     ExercisesModule,
