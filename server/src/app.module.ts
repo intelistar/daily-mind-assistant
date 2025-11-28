559b71e5e94783a224f1f9d9c95c4fdcbc5dd661
nekit <garkusha500@gmail.com>
2025-11-28 23:50:55 +0300

    a

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,6 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ExercisesModule } from './modules/exercises/exercises.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { TelegramModule } from './modules/telegram/telegram.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
@@ -12,7 +11,6 @@ import { ScheduleModule } from '@nestjs/schedule';
     AuthModule,
     ExercisesModule,
     TasksModule,
-    TelegramModule,
     ScheduleModule.forRoot(),
   ],
 })
