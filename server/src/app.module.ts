b8fd1e8281f45855db728d8f2dc86c8d1003099e
nekit <garkusha500@gmail.com>
2025-12-06 01:53:55 +0300

    feat: add health module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ExercisesModule } from './modules/exercises/exercises.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     ExercisesModule,
     TasksModule,
     ScheduleModule.forRoot(),
+    HealthModule,
   ],
 })
 export class AppModule {}
