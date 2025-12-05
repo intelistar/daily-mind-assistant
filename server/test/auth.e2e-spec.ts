b2e24a86825efaee33b5923cf40147b9ff1f0c5a
nekit <garkusha500@gmail.com>
2025-12-06 02:09:48 +0300

    feat: change e2e tests

--- server/test/auth.e2e-spec.ts
+++ server/test/auth.e2e-spec.ts
@@ -1,7 +1,28 @@
-import * as request from 'supertest';
+// import * as request from 'supertest';
+
+// describe('Auth (staging)', () => {
+//   const api = request(process.env.STAGING_URL!);
+
+//   it('/auth/register POST', async () => {
+//     const dto = {
+//       name: 'Test User',
+//       email: `test_${Date.now()}@mail.com`,
+//       password: '11111111',
+//     };
+
+//     const res = await api.post('/auth/register').send(dto);
+
+//     expect(res.status).toBe(201);
+//   });
+// });
+/* eslint-disable */
+import axios from 'axios';
 
 describe('Auth (staging)', () => {
-  const api = request(process.env.STAGING_URL!);
+  const api = axios.create({
+    baseURL: process.env.STAGING_URL,
+    withCredentials: true,
+  });
 
   it('/auth/register POST', async () => {
     const dto = {
@@ -10,7 +31,7 @@ describe('Auth (staging)', () => {
       password: '11111111',
     };
 
-    const res = await api.post('/auth/register').send(dto);
+    const res = await api.post('/auth/register', dto);
 
     expect(res.status).toBe(201);
   });
